2519b47c79a0a2b662208db9f4553c23a5173c88
shohan89 <maffin.killer007@gmail.com>
2025-07-31 01:11:30 +0600

    fix broken (Tailwind) issue.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import './App.css'
 import './index.css'
-import App from './App.jsx'
+import Blogs from './pages/Blogs.jsx'
+import Home from './pages/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/blogs',
+    element: <Blogs />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
