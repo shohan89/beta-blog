37c741c22f39fed975d964aa7acfe1c4873b978c
shoh4n <maffin.killer007@gmail.com>
2025-07-31 13:50:13 +0600

    feat: add Footer comp

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+            <aside>
+                <p>Copyright © {new Date().getFullYear()} - All right reserved by BetaBlog</p>
+            </aside>
+        </footer>
+    );
+};
+
+export default Footer;
