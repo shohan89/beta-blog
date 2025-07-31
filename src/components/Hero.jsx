d09bc980b43d3b45ae639853ae519420b93979df
shoh4n <maffin.killer007@gmail.com>
2025-07-31 17:22:42 +0600

    feat: add button after hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 
 const Hero = () => {
     return (
@@ -11,12 +12,24 @@ const Hero = () => {
             <div className="hero-overlay"></div>
             <div className="hero-content text-neutral-content text-center">
                 <div className="max-w-lg">
-                    <h1 className="mb-5 text-5xl font-bold">Welcome to BetaBlog</h1>
+                    <h1 className="mb-5 text-5xl font-bold">Welcome to <span>BetaBlog</span></h1>
                     <p className="mb-5">
                         BetaBlog is the bridge between the complex world of technology and the curious minds eager to understand it
                     </p>
-                    <button className="btn btn-primary mr-4">Read Blogs</button>
-                    <button className="btn btn-primary">Bookmarks</button>
+                    <Link to="/blogs" class="px-5 py-2.5 relative rounded group font-medium text-white font-medium inline-block mr-4">
+                        <span class="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
+                        <span class="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
+                        <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
+                        <span class="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
+                        <span class="relative">Read Blogs</span>
+                    </Link>
+                    <Link to="/bookmarks" class="px-5 py-2.5 relative rounded group font-medium text-white font-medium inline-block">
+                        <span class="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
+                        <span class="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
+                        <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
+                        <span class="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
+                        <span class="relative">Bookmarks</span>
+                    </Link>
                 </div>
             </div>
         </div>
