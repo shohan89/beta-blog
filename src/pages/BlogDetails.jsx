d4c043d6dd7d57ba37f293650bbfc2c1953a5cc5
shohan89 <maffin.killer007@gmail.com>
2025-08-01 21:34:17 +0600

    feat: add toggle content & author

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useLoaderData } from "react-router";
+import { Link, Outlet, useLoaderData } from "react-router";
 
 const BlogDetails = () => {
     const [tabIndex, setTabIndex] = useState(0);
@@ -16,13 +16,19 @@ const BlogDetails = () => {
                     </div>
                     {/* Tabs */}
                     <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap">
-                        <Link onClick={()=> setTabIndex(0)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 0 ? "border border-b-0" : "border-b"} border-gray-200`}>
+                        <Link 
+                        to=''
+                        onClick={()=> setTabIndex(0)} 
+                        className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 0 ? "border border-b-0" : "border-b"} border-gray-200`}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                                 <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                             </svg>
                             <span>Content</span>
                         </Link>
-                        <Link onClick={()=> setTabIndex(1)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 1 ? 'border border-b-0' : 'border-b'} rounded-t-lg border-gray-200`}>
+                        <Link 
+                        to={'author'}
+                        onClick={()=> setTabIndex(1)} 
+                        className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 1 ? 'border border-b-0' : 'border-b'} rounded-t-lg border-gray-200`}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                                 <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                                 <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
@@ -32,6 +38,7 @@ const BlogDetails = () => {
                     </div>
                     {/* Tabs */}
                 </div>
+                <Outlet />
             </article>
             <div>
                 <div className="flex flex-wrap py-6 gap-2 border-t border-dashed border-gray-400 dark:border-gray-600">
