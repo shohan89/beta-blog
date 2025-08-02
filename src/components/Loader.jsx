8335215bfaf2cee7724103bb4086f12564a13a86
shoh4n <maffin.killer007@gmail.com>
2025-08-02 11:36:08 +0600

    feat: add loader comp

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import { PacmanLoader } from 'react-spinners';
+
+const Loader = () => {
+    return (
+        <div className='flex flex-col justify-center items-center'>
+            <PacmanLoader />
+        </div>
+    );
+};
+
+export default Loader;
