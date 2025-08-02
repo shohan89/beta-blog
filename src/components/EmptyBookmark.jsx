bed7c99aae4b477ca6569254efba258e5aa7da90
shoh4n <maffin.killer007@gmail.com>
2025-08-02 17:02:48 +0600

    fix minor issues on various files

--- src/components/EmptyBookmark.jsx
+++ src/components/EmptyBookmark.jsx
@@ -4,12 +4,12 @@ const EmptyBookmark = ({ message, address, label }) => {
     return (
         <div className='min-h-[calc(100vh-116px)] flex flex-col gap-5 justify-center items-center pb-16'>
             <p>{message}</p>
-            <Link to={address} class="px-5 py-2.5 relative rounded group font-medium text-white font-medium inline-block mr-4">
-                <span class="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
-                <span class="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
-                <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
-                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
-                <span class="relative">{label}</span>
+            <Link to={address} className="px-5 py-2.5 relative rounded group text-white font-medium inline-block mr-4">
+                <span className="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
+                <span className="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
+                <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
+                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
+                <span className="relative">{label}</span>
             </Link>
         </div>
     );
