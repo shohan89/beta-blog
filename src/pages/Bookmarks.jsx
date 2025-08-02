48779c6823a22966bd447a0b225ebce678aa6bd3
shoh4n <maffin.killer007@gmail.com>
2025-08-02 13:11:42 +0600

    feat: show all bookmarks from LS

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
+import BlogCard from "../components/BlogCard";
+import { getBlogsFromLS } from "../utils";
 
 const Bookmarks = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(()=>{
+        const storedBlogs = getBlogsFromLS();
+        setBlogs(storedBlogs);
+    },[])
+
     return (
-        <div>
-            <h2>This is bookmarks</h2>
+        <div className="grid py-12 px-4 justify-center grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+            {
+                blogs.map(blog => <BlogCard key={blog.id} blog={blog} />)
+            }
         </div>
     );
 };
