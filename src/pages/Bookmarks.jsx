996bdcd2e34d3a292c0d8eea265fd8c86d5ae170
shoh4n <maffin.killer007@gmail.com>
2025-08-02 16:35:29 +0600

    feat: Add no bookmark comp

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import BlogCard from "../components/BlogCard";
+import EmptyBookmark from "../components/EmptyBookmark";
 import { deleteBlog, getBlogsFromLS } from "../utils";
 
 const Bookmarks = () => {
@@ -15,7 +16,14 @@ const Bookmarks = () => {
         const storedBlogs = getBlogsFromLS();
         setBlogs(storedBlogs);
     }
-
+    if(blogs.length < 1) {
+        return <EmptyBookmark 
+        message={'No bookmark available!'}
+        address={'/blogs'}
+        label={'Browse Blogs'}
+        />
+    
+    }
     return (
         <div className="grid py-18 justify-center grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
             {
